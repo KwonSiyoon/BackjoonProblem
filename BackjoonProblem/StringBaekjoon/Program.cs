953dfb3861b052f54a37d8fa0a6e9676dda35c76
KwonSiyoon <140003447+KwonSiyoon@users.noreply.github.com>
2024-06-25 09:53:47 +0900

    0624
    문자열 5번까지 품

---
+++ BackjoonProblem/StringBaekjoon/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBaekjoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+/*
+            SumOfNumbers();
+            void SumOfNumbers() // 11720
+            {
+                int a = int.Parse(Console.ReadLine());
+                string nums = Console.ReadLine();
+                int sum = 0;
+                for (int i = 0; i < a; i++)
+                {
+                    int num = Convert.ToInt32(nums[i]) - Convert.ToInt32('0');
+                    sum += num;
+                }
+                Console.WriteLine(sum);
+            }
+*/
+
+/*
+            ASCII();
+            void ASCII() // 11654
+            {
+                Console.WriteLine((int)Console.ReadLine()[0]);
+            }
+*/
+
+/*
+            String();
+            void String() // 9086
+            {
+                int a = int.Parse(Console.ReadLine());
+                for(int i = 0; i < a; i++)
+                {
+                    string input = Console.ReadLine();
+                    Console.WriteLine(input[0] +""+ input[input.Length-1]);
+                }
+            }
+*/
+
+/*
+            StringLength();
+            void StringLength() // 2743
+            {
+                string input = Console.ReadLine();
+                Console.WriteLine(input.Length);
+            }
+*/
+
+
+/*
+            CharAndString();
+            void CharAndString() // 27866
+            {
+                string read = Console.ReadLine();
+                List<char> list = new List<char>();
+                foreach(char a in read)
+                {
+                    list.Add(a);
+                }
+                Console.WriteLine(list[int.Parse(Console.ReadLine())-1]);
+            }
+            CharAndString2();
+            void CharAndString2()
+            {
+                string input = Console.ReadLine();
+                Console.WriteLine(input[int.Parse(Console.ReadLine())-1]);
+            }
+*/
+        }
+    }
+}
