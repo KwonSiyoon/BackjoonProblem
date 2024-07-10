4210688b891f8208d217734d6a69fbe1cf21525e
KwonSiyoon <140003447+KwonSiyoon@users.noreply.github.com>
2024-07-11 08:44:53 +0900

    0711

---
+++ BackjoonProblem/TwoDimensionalArray/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoDimensionalArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+/*
+            PlusMatrix();
+            void PlusMatrix() // 2738
+            {
+                string[] input = Console.ReadLine().Split();
+                int n = int.Parse(input[0]);
+                int m = int.Parse(input[1]);
+                int[,] a = new int[n, m];
+                int[,] b = new int[n, m];
+                for(int i = 0; i < n; i++)
+                {
+                    string[] temp = Console.ReadLine().Split();
+                    for(int j=0; j < m; j++)
+                    {
+                        a[i, j] = int.Parse(temp[j]);
+                    }
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    string[] temp = Console.ReadLine().Split();
+                    for (int j = 0; j < m; j++)
+                    {
+                        b[i, j] = int.Parse(temp[j]);
+                    }
+                }
+                int[] sum = new int[m];
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        sum[j] = a[i, j] + b[i, j];
+                        Console.Write(sum[j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+*/
+        }
+    }
+}
