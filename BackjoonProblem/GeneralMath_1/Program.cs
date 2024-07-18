0afa83fa117f564b59579e4a21bca8e5cc287e4b
KwonSiyoon <140003447+KwonSiyoon@users.noreply.github.com>
2024-07-18 09:38:34 +0900

    0718

--- BackjoonProblem/GeneralMath_1/Program.cs
+++ BackjoonProblem/GeneralMath_1/Program.cs
@@ -11,8 +11,49 @@ namespace GeneralMath_1
         static void Main(string[] args)
         {
 
+
+
+
+            BaseConversion_2();
+            void BaseConversion_2() // 11005
+            {
+                string[] input = Console.ReadLine().Split();
+                int num = int.Parse(input[0]);
+                int baseI = int.Parse(input[1]);
+                List<char> list = new List<char>();
+                int remain = 0;
+                // 주어진 num을 baseI로 나눈 나머지를 구한다.
+                // 나머지 값을 문자로 변환해서 list에 추가한다.
+                // 주어진 num을 baseI로 나눈다
+                while (num > 0)
+                {
+                    remain = num % baseI;
+                    if(remain < 10)
+                    {
+                        remain += 48;
+                        list.Add((char)remain);
+                    }
+                    else if(remain>=10)
+                    {
+                        remain += 55;
+                        list.Add((char)remain);
+                    }
+                    num = num / baseI;
+                }
+                for(int i = list.Count-1; i >=0; i--)
+                {
+                    Console.Write(list[i]);
+                }
+            }
+
+
+
+
+
+
+/*
             BaseConversion();
-            void BaseConversion()
+            void BaseConversion() // 2745
             {
                 // A~Z는 65 ~ 90
                 // -55를 하면 10 ~ 35
@@ -33,7 +74,7 @@ namespace GeneralMath_1
 
                 Console.WriteLine(sum);
             }
-
+*/
 
         }
     }
