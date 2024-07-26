8c2ad528720604a42b1d617333b5d09112d51ca6
KwonSiyoon <140003447+KwonSiyoon@users.noreply.github.com>
2024-07-26 09:37:28 +0900

    0726 Finish DivisorMultipleDecimal

--- BackjoonProblem/DivisorMultipleDecimal/Program.cs
+++ BackjoonProblem/DivisorMultipleDecimal/Program.cs
@@ -12,14 +12,115 @@ namespace DivisorMultipleDecimal
         {
 
 
+            /*Factorization();
+            void Factorization() // 소인수분해 // 11653
+            {
+                int a = int.Parse(Console.ReadLine());
+                while(a > 1)
+                {
+                    for(int i = 2; i<=a; i++)
+                    {
+                        if(a%i == 0)
+                        {
+                            Console.WriteLine(i);
+                            a /= i;
+                            break;
+                        }
+                    }
+                }
+            }*/
 
 
 
+            /*Decimal();
+            void Decimal() // 2581
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                List<int> decimalList = new List<int>();
+                for(int i = a; i <= b; i++)
+                {
+                    int cnt = 0;
+                    for (int j = 1; j <= i; j++)
+                    {
+                        if (i % j == 0) cnt++;
+                        if (cnt >= 3) break;
+                        if(i==j && cnt == 2)
+                        {
+                            decimalList.Add(i);
+                        }
+                    }
+                }
+                if(decimalList.Count == 0) Console.WriteLine(-1);
+                else
+                {
+                    int sum = 0;
+                    foreach(var c in decimalList)
+                    {
+                        sum += c;
+                    }
+                    Console.WriteLine(sum);
+                    Console.WriteLine(decimalList[0]);
+                }
+            }*/
 
 
+            /*FindDecimal();
+            void FindDecimal()
+            {
+                int n = int.Parse(Console.ReadLine());
+                string[] input = Console.ReadLine().Split();
+                int ans = 0;
+                for(int i = 0; i < n; i++)
+                {
+                    int temp = int.Parse(input[i]);
+                    int cnt = 0;
+                    for(int j = 1; j <= temp; j++)
+                    {
+                        if(temp%j == 0)
+                        {
+                            cnt++;
+                        }
+                        if (cnt >= 3) break;
+                        if (j == temp && cnt == 2) ans++;
+                    }
+                }
+                Console.WriteLine(ans);
+            }*/
 
-
-
+/*
+            SumOfDivisors();
+            void SumOfDivisors() // 9506
+            {
+                while (true)
+                {
+                    int a = int.Parse(Console.ReadLine());
+                    if (a == -1) break;
+                    List<int> divisorList = new List<int>();
+                    for(int i = 1; i < a; i++)
+                    {
+                        if (a % i == 0) divisorList.Add(i);
+                    }
+                    bool isPerfect = false;
+                    int sum = 0;
+                    foreach(var c in divisorList)
+                    {
+                        sum += c;
+                    }
+                    if (a == sum) isPerfect = true;
+                    if(!isPerfect) Console.WriteLine(a + " is NOT perfect.");
+                    else if (isPerfect)
+                    {
+                        Console.Write($"{a} = {divisorList[0]}");
+                        for(int i = 1; i < divisorList.Count; i++)
+                        {
+                            Console.Write(" + " + divisorList[i]);
+                        }
+                        Console.WriteLine();
+                    }
+                }
+            }
+*/
 
             /*FindDivisor();
             void FindDivisor()
